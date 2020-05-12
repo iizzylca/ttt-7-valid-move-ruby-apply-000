071d1d1e883efbef86611e22dc8d0d9ea5a7cb70
Israel Canessa <israellcanessa@gmail.com>
2020-05-12 02:08:43 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 def valid_move?(board, index)
-  spot= index.to_i - 1
-	if !position_taken?(board, spot) && spot.between?(0,8)
+  
+	if !position_taken?(board, index) && index.between?(0,8) 
 		true
   else
 		false
